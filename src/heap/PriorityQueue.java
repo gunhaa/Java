32844f8c931c83e9b015bd14dc6ac2081295938c
gunha <wh8299@gmail.com>
2025-06-23 12:23:33 +0900

    feat: priorityQueue , heap 추가

---
+++ src/heap/PriorityQueue.java
@@ -0,0 +1,15 @@
+package heap;
+
+public interface PriorityQueue {
+    /*
+    큐와 비슷하지만, 우선순위가 높은 것부터 나온다
+    주요 메서드:  insert(우선 순위 삽입, 정보 필요)
+                delete(최상위 우선순위를 반환하며 제거)
+                peek(제거하지 않으며 우선순위 최상위를 반환)
+
+    우선순위 큐는 "개념"이고, - ADT (priority Queue)
+    힙은 우선순위 큐를 "효율적으로 구현하는 방법" 중 하나다. - Data Structure (heap)
+    Priority Queue를 구현하는 방식 중, 가장 성능이 좋은 방식이 heap이기 때문에 Priority Queue = Heap이라고 생각을 하는 것이다
+    프로세스 스케쥴링에서 주로 사용된다
+    */
+}
